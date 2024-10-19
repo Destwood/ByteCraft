fdabb9cd998b798462574769b1ce6c08388c0f22
Vindener <vindener12@gmail.com>
2024-10-19 15:21:44 +0300

    Adedd fundraiser

--- src/components/screens/fundraiser/Fundraiser.jsx
+++ src/components/screens/fundraiser/Fundraiser.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import classes from "./Fundraiser.module.scss"
 import userAvatar from './user-avatar.svg'
 import ProgressBar from './ProgressBar';
+import statisticIcon from '../home/statistic-icon.svg'
+import copyIcon from './copy-icon.svg'
 
 const Fundraiser=()=> {
     const [progress, setProgress] = useState(50);
@@ -27,8 +29,25 @@ const Fundraiser=()=> {
                 <div className={classes["main-info-block"]}>
                     <p>Зібрано, грн</p> <p>1000 / 10000</p>
                 </div>
+
                 <ProgressBar progress={progress} />
 
+                <div className={classes["main-info-block"]}>
+                    <p>Дата завершення збору</p> <p className={classes.date}>10.10.2024</p>
+                </div>
+                <p className={classes.text}><img src={statisticIcon} alt="Icon" />1024 учасників</p>
+                <p> Організатор збору:</p>
+
+                <div className={classes["organizer-block"]}>
+                    <img src={userAvatar} className={classes["user-avatar"]} alt="Avatar" />
+                    <p className={classes["user-name"]}>Волонтер1</p>
+                </div>
+                
+                <input className={classes.sum} placeholder='Сума донату' type="number"></input>
+                <button className={classes.send}>Залатити через гугл</button>
+
+                <p>Для поширення інформації про збір: </p>
+                <button className={classes.copy}><img src={copyIcon} alt="Icon" />Скопіювати ссилку</button>
             </div>
 
             <div className={classes.info}>
@@ -53,7 +72,20 @@ const Fundraiser=()=> {
                     <p className={classes["user-sum"]}>100 грн</p>
                 </div>
 
-                 <div className={classes.user}>
+                <div className={classes.user}>
+                    <img src={userAvatar} className={classes["user-avatar"]} alt="Avatar" />
+
+                    <p className={classes["user-name"]}>  ВолонтерN</p>
+                    <p className={classes["user-sum"]}>100 грн</p>
+                </div>
+
+                <div className={classes.user}>
+                    <img src={userAvatar} className={classes["user-avatar"]} alt="Avatar" />
+
+                    <p className={classes["user-name"]}>  ВолонтерN</p>
+                    <p className={classes["user-sum"]}>100 грн</p>
+                </div>
+                <div className={classes.user}>
                     <img src={userAvatar} className={classes["user-avatar"]} alt="Avatar" />
 
                     <p className={classes["user-name"]}>  ВолонтерN</p>
